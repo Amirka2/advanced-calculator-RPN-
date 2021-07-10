46b1c57652bad6dbb5059d22598d857cd4803134
Amir <far20022002213@gmail.com>
2021-07-10 20:23:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advanced_calculator__RPN_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
