6bff939a9187a815063fbd28cf7efb3d72609417
Amir <far20022002213@gmail.com>
2021-07-14 23:03:19 +0500

    calculator works

--- Program.cs
+++ Program.cs
@@ -17,8 +17,15 @@ namespace advanced_calculator__RPN_
                 Console.WriteLine("Некорректное выражение!");
                 return;                                             /*можно так завершать?*/
             }
-            Console.WriteLine("все ок");
-            Console.ReadKey();
+            // making array with input elements 
+            while (input.Contains("  ")) { input = input.Replace("  ", " "); }
+            input = input.Trim();
+            string[] inputArray = input.Split(' ');
+
+            var result = Calculate(inputArray[0], inputArray[1], inputArray[2]);
+
+            Console.WriteLine("result = " + result);
+            WriteResult(result);
         }
         static string GetInput()
         {
@@ -65,6 +72,29 @@ namespace advanced_calculator__RPN_
             if (digits.Length > 1) return false;
             if ((x == "+") || (x == "-") || (x == "*") || (x == "/")) return true;
             return false;
+        }
+        static double Calculate(string x, string op, string y)
+        {
+            double x1 = Convert.ToDouble(x);
+            double x2 = Convert.ToDouble(y);
+            double result;
+            if (op == "+") result = x1 + x2;
+            else if (op == "-") result = x1 - x2;
+            else if (op == "*") result = x1 * x2;
+            else if (op == "/") result = x1 / x2;
+            else
+            {
+                Console.WriteLine("некорректная операция");
+                result = 0;
+            }
+            return result;
+        }
+        static void WriteResult(double result)
+        {
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                sw.WriteLine(result);
+            }
         }
     }
 }
